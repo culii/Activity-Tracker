a4bd2bc52646d6e4fb6d07d20272f7abe514a522
culii <michaelskes@gmail.com>
2023-08-10 13:27:39 +0100

    added empty ActivityDetails component, added basic fetch for all activites, added proxy

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+// components
+//import ActivityDetails from "../components/ActivityDetails";
+
 const Home = () => {
+  const [activities, setActivities] = useState(null);
+
+  useEffect(() => {
+    const fetchActivities = async () => {
+      const response = await fetch("/api/activities");
+      const json = await response.json();
+
+      if (response.ok) {
+        setActivities(json);
+      }
+    };
+    fetchActivities();
+  }, []);
   return (
-    <div className="Home">
-      <h2>Home</h2>
+    <div className="home">
+      <div className="activities">
+        {activities &&
+          activities.map((activity) => (
+            <p key={activity._id}>{activity.name}</p>
+          ))}
+      </div>
     </div>
   );
 };
